a9cb52fa35aaf2bfa2638088329686182b295750
Kattis91 <katya.svedmark@gmail.com>
2023-08-25 13:52:09 +0000

    Add function to get user's name and choice whether to play or not

--- run.py
+++ run.py
@@ -1,1 +1,12 @@
 import random
+
+def get_user_info():
+    """
+    Gives a user the opportunity to enter a username and to choose whether
+    to start the game or to quit
+    """
+    username = input("Please enter your name: ")
+    print(f"\nHello {username}, are you ready to play?\n")
+    user_choice = input("yes / no: ")
+
+get_user_info()
