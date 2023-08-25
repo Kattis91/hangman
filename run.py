e556c90a58a91e729662cd8c3564b28fafc07a03
Kattis91 <katya.svedmark@gmail.com>
2023-08-25 14:12:16 +0000

    Add a while loop to get_user_info function which will run until we get either 'yes' or 'no'

--- run.py
+++ run.py
@@ -2,11 +2,24 @@ import random
 
 def get_user_info():
     """
-    Gives a user the opportunity to enter a username and to choose whether
-    to start the game or to quit
+    Gives a user the opportunity to enter a username. 
+    Runs a while loop to collect data from the user, which must be yes or no. 
+    The loop will repeatedly request data until we get yes or no.
     """
     username = input("Please enter your name: ")
     print(f"\nHello {username}, are you ready to play?\n")
     user_choice = input("yes / no: ")
+
+    while True:
+        if user_choice.lower() == "no":
+            print("\nYou are welcome back another time. Have a nice day!")
+            break
+        elif user_choice.lower() == "yes":
+            print("\nLet's go then...")
+            break
+        else:
+            print(f"\nYou entered {user_choice}, please enter 'yes' or 'no'\n")
+            user_choice = input("yes / no: ")
+            continue   
 
 get_user_info()
